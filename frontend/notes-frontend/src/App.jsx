35a5442b1716ab6ef6d8ff3aefba6f2a353d4a7c
Duverger PETGA <epegase@gmail.com>
2022-11-28 12:10:24 +0000

    add auth pages

--- frontend/notes-frontend/src/App.jsx
+++ frontend/notes-frontend/src/App.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { AppContext } from "./lib/contextLib";
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          Scratch
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Scratch
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+          {isAuthenticated ? (
+          <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+          ) : (
+          <>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </>
+          )}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
