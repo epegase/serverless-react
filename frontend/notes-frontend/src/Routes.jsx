8a42b0eb15c79190e98bfc208c6d691ac0e36145
Duverger PETGA <epegase@gmail.com>
2022-12-06 14:25:22 +0000

    update frontend

--- frontend/notes-frontend/src/Routes.jsx
+++ frontend/notes-frontend/src/Routes.jsx
@@ -4,7 +4,8 @@ import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
-
+import NewNote from "./containers/NewNote";
+import Settings from "./containers/Settings";
 
 export default function Links() {
   return (
@@ -12,6 +13,8 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/notes/new" element={<NewNote />} />
       <Route path="*" element={<NotFound />} />;
     </Routes>
   );
